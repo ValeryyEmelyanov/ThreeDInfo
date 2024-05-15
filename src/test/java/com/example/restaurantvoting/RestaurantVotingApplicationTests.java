4f42ca3a0a6033d6c8e2775fba659bda04073e4c
Valery Emelyanov <westbrookokc1997@gmail.com>
2024-05-16 00:45:25 +0300

    0.1

---
+++ src/test/java/com/example/restaurantvoting/RestaurantVotingApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.restaurantvoting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootTest
+class RestaurantVotingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
